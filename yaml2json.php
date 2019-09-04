a47f9177f8a520ff7d20ddb4064b1a2f430ae224
PJ Eby <pje@telecommunity.com>
2019-09-04 01:33:26 -0400

    Update deps & remove unneeded PARSE_OBJECT flag

--- yaml2json.php
+++ yaml2json.php
@@ -3,10 +3,7 @@ require_once __DIR__ . '/vendor/autoload.php';
 use Symfony\Component\Yaml\Yaml;
 
 echo json_encode(
-	Yaml::parse(
-		file_get_contents('php://stdin'),
-		Yaml::PARSE_OBJECT_FOR_MAP | Yaml::PARSE_OBJECT
-	)
+	Yaml::parse( file_get_contents('php://stdin'), Yaml::PARSE_OBJECT_FOR_MAP )
 );
 
 echo "\n";
